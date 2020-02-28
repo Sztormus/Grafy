28515e82ad2ffa4fefda6c39a2a8e3c0487a2bef
zuczeks <50636761+zuczeks@users.noreply.github.com>
2020-02-28 22:36:56 +0100

    Created basic library for graph representations

---
+++ Projekt 1/1/GrafyVS.cpp
@@ -0,0 +1,34 @@
+﻿//Krótka biblioteka do obslugi roznych reprezentacji grafow.
+//
+
+#include "pch.h"
+#include <iostream>
+#include "AdjacencyList.h"
+#include "AdjacencyMatrix.h"
+#include "IncidenceMatrix.h"
+
+using namespace std;
+
+int main()
+{
+	AdjacencyMatrix* g = new AdjacencyMatrix(5);
+	IncidenceMatrix* i = new IncidenceMatrix(5);
+	AdjacencyList* h = new AdjacencyList(5);
+	g->setEdge(2, 3);
+	g->setEdge(4, 1);
+	g->setEdge(0, 3);
+	i->setEdge(2, 3);
+	i->setEdge(2, 4);
+	i->setEdge(2, 1);
+	i->setEdge(0, 2);
+	h->setEdge(2, 3);
+	h->setEdge(1, 3);
+	h->setEdge(0, 4);
+	h->setEdge(4, 2);
+
+	g->print();
+	i->print();
+	h->print();
+
+	return 0;
+}
