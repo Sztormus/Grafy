225ba151b55070e4e90d939f39a41c30bf6e9074
zuczeks <50636761+zuczeks@users.noreply.github.com>
2020-05-06 21:32:21 +0200

    2 done

---
+++ Projekt 5/2/IncidenceMatrix.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "GraphRepresentation.h"
+
+class IncidenceMatrix : public GraphRepresentation
+{
+public:
+	IncidenceMatrix(int n);
+	~IncidenceMatrix();
+	void setEdge(int node1, int node2);
+	int edgeExists(int node1, int node2);
+	void print();
+};
